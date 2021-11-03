dd96ec91be975fbd34fcc478fa1adb9729f37395
danhergoz <88807722+danhergoz@users.noreply.github.com>
2021-11-03 10:58:29 -0500

    Add files via upload

---
+++ authProject/urls.py
@@ -0,0 +1,33 @@
+"""authProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from authApp import views
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('user/', views.UserCreateView.as_view()),
+    path('user/<int:pk>/', views.UserDetailView.as_view()),
+    path('nomina/', views.nominaCreateView.as_view()),
+    path('nominas/myNomina/<int:user>/', views.ListNominasView.as_view()),
+    path('nominas/newNomina/<int:user>/', views.CreateNominaView.as_view()),
+    path('nominas/deletNomina/<int:user>/<int:pk>', views.nominaDeleteView.as_view())
+]
+
+from django.views.generic import RedirectView
+from django.conf.urls import url
+
